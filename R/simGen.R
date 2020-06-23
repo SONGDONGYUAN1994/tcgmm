7416758e0c27ab70354d5ff2cc6cbe4bf7118ff1
songdongyuan <dsong@hsph.harvard.edu>
2020-06-23 09:52:40 -0700

    First commit pkg

---
+++ R/simGen.R
@@ -0,0 +1,79 @@
+#' @title Generate Simulated Data of Two-Condition Gaussian
+#'
+#' @description
+#' This function generates multivariate Gaussian distributed data under case/control conditions.
+#' The basic assumption is that for each type, the mean has a shift while the variance keeps the same cross conditions.
+#'
+#' @param n The sample size of simulated data (cells)
+#' @param n.feature Number of features (genes)
+#' @param n.group Number of underlying groups (cell types)
+#' @param type.prop A vector of same length as \code{n.group}. The proportion of each group.
+#' @param con.prop Proportion of condition 1. Default is 0.5.
+#' @param mu.mat A matrix which is \code{n.feature} by \code{n.group}. Define the mean parameter of multivariate Gaussian in condition 1.
+#' @param sigma.mat A matrix which is \code{n.feature} by \code{n.group}. Define the standard deviation parameter of multivariate Gaussian.
+#' @param delta.mat A matrix which is \code{n.feature} by \code{n.group}. Define the shift of mean of multivariate Gaussian.
+#'
+#' @return
+#' A list which contains the data, the group type and the condition.
+#' ## y The feature of each data point
+#' ## z The group type of each data point
+#' ## g The condtion (condition1 / condition 2) of each data point
+#'
+#' @examples
+#' mu1 <- c(5, 7, 9)
+#' theta1 <- c(1, 2, 0)
+#' sigma1 <- c(1, 2, 3)
+#' mu2 <- c(10, 15, 4)
+#' theta2 <- c(1, 2, 6)
+#' sigma2 <- c(0.4, 0.2, 0.4)
+#' mu.mat <- cbind(mu1, mu2)
+#' delta.mat <- cbind(theta1, theta2)
+#' sigma.mat <- cbind(sigma1, sigma2)
+#' dat <- simGen(n = 100, n.feature = 2, n.group = 3, type.prop = c(0.2, 0.3, 0.5),
+#' mu.mat = mu.mat, sigma.mat = sigma.mat, delta.mat = delta.mat)
+#' @importFrom extraDistr rmnom
+#' @importFrom mvtnorm rmvnorm dmvnorm
+#' @export
+#' @author Dongyuan Song
+
+simGen <- function(n, n.feature, n.group, type.prop, con.prop = 0.5, mu.mat, sigma.mat, delta.mat) {
+
+  ## Check dimension
+  stopifnot(length(type.prop) == n.group)
+  stopifnot(dim(mu.mat)[1] == n.group)
+  stopifnot(dim(mu.mat)[2] == n.feature)
+  stopifnot(dim(mu.mat) == dim(sigma.mat))
+  stopifnot(dim(mu.mat) == dim(delta.mat))
+
+  ## latend variable
+  z <- rmnom(n = n, size = 1, prob = type.prop)
+
+  ## true group variable
+  g <- c(rep(0, round(n*con.prop)), rep(1, n - round(n*con.prop)))
+
+  ## multivariate Gaussian
+  mu.mat <- data.matrix(mu.mat)
+  sigma.mat <- data.matrix(sigma.mat)
+
+  s_control <- (lapply(1:n.group, function(x){
+    mu <- mu.mat[x, ]
+    sigma <- diag(sigma.mat[x, ])
+    rmvnorm(n = n, mean = mu, sigma = sigma)
+  }))
+
+  s_case <- (lapply(1:n.group, function(x){
+    mu <- mu.mat[x, ] + delta.mat[x, ]
+    sigma <- diag(sigma.mat[x, ])
+    rmvnorm(n = n, mean = mu, sigma = sigma)
+  }))
+
+  y <- lapply(1:n.group, function(x){
+    z[, x]*s_control[[x]]*(1-g) + z[, x]*s_case[[x]]*g
+  })
+
+  y <- Reduce('+', y)
+
+  res <- list(y = y, z = z, g = g)
+
+  return(res)
+}
---
+++ R/tcGmmPen.R
@@ -0,0 +1,128 @@
+#' @title Fit Two-Condition Gaussian Mixture Model
+#'
+#' @description
+#' This function fits the Two-Condition Gaussian Mixture Model (TCGMM).
+#' We assume that the latent groups are consistent between two conditions only with shifts in mean parameters.
+#'
+#' @param y A matrix with rows as samples (cells) and columns as features (genes)
+#' @param g A vector indicating condition 1 (0) and condition 2 (1)
+#' @param zInit A matrix indicating the assignment of groups with rows as samples and columns as groups
+#' @param maxIter A numeric value of maximum iteration number. Default is 100.
+#' @param thresh A numeric value of the converge criteria. Define as the Frobenius norm of the difference of current mean and mean in last iteration. Default is 1e-8.
+#' @param lambda A numeric value of the lambda parameter for LASSO. Larger lambda means harder penalization on the shift parameter.
+#' @param verboseN A llogical value. Whether to print the iteration number.
+#'
+#' @return
+#' A list which contains the fitting result.
+#' ## mu The mean parameter
+#' ## sigma The standard deviation parameter
+#' ## delta The shift of mean parameter
+#' ## z The assignment of groups
+#' ## model The fitted regression model of each group
+#'
+#' @importFrom extraDistr rmnom
+#' @importFrom mvtnorm rmvnorm dmvnorm
+#' @importFrom stats coef glm
+#' @export
+#' @author Dongyuan Song
+
+tcGmmPen <- function(y, g, zInit, maxIter = 100, thresh = 1e-16, lambda = 1, verboseN = TRUE) {
+
+  ## Set dimension
+  n <- dim(y)[1]
+  n.feature <- dim(y)[2]
+  n.group <- dim(zInit)[2]
+
+  for(k in 1:maxIter) {
+    if(k == 1) {
+      z <- zInit
+      gamma_curr <- zInit
+
+      dat_all <- cbind(y, g, z)
+
+      dat_list <- lapply(1:n.group, function(j){
+        dat_all[z[, j] == 1, ]
+      })
+
+      fit_list <- lapply(1:n.feature, function(i) {
+        lapply(1:n.group, function(j){
+          dat <- dat_list[[j]]
+
+          fit <- glm(dat[, i] ~ dat[, n.feature + 1], family = "gaussian")
+          fit
+        })
+      })
+    }
+    else {
+      dat_all <- cbind(y, g, gamma_curr, z)
+      fit_list <- lapply(1:n.feature, function(i) {
+        lapply(1:n.group, function(j){
+          dat <- dat_all
+          fit <- glmnet::glmnet(y = dat[, i], x = cbind(dat[, n.feature + 1], 0), family = "gaussian", weights = dat[, n.feature + 1 + j], alpha = 1, lambda = lambda)
+          fit
+        })
+      })
+    }
+    if(k >= 2) mu_old <- mu_curr
+
+    ## Extract fitting values
+    mu_curr <- sapply(1:n.feature, function(i) {
+      sapply(1:n.group, function(j){
+        coef(fit_list[[i]][[j]])[1]
+      })
+    })
+
+    delta_curr <- sapply(1:n.feature, function(i) {
+      sapply(1:n.group, function(j){
+        coef(fit_list[[i]][[j]])[2]
+      })
+    })
+    sigma_curr <- sapply(1:n.feature, function(i) {
+      sapply(1:n.group, function(j){
+        stats::sigma(fit_list[[i]][[j]])
+      })
+    })
+
+    p_curr <- colMeans(gamma_curr)
+
+    gamma_curr <- apply(dat_all, 1, function(x){
+
+      y_i <- x[1:n.feature]
+      g_i <- x[n.feature + 1]
+
+      ## Calculate density
+      if(g_i == 0) {
+        d <- sapply(1:n.group, function(i){
+          dmvnorm(y_i, mean = mu_curr[i, ], sigma = diag(sigma_curr[i, ]))
+        })}
+      else {
+        d <- sapply(1:n.group, function(i){
+          dmvnorm(y_i, mean = mu_curr[i, ] + delta_curr[i, ], sigma = diag(sigma_curr[i, ]))})
+      }
+      gamma <- sapply(1:n.group, function(i){
+        p_curr[i]*d[i]/(sum(p_curr*d))
+      })
+      gamma
+    }
+    )
+    gamma_curr <- t(gamma_curr)
+
+    if(verboseN) {
+      cat(paste0("Iteration ", k, "\n"))
+    }
+
+    if(k >= 2 &&  norm(mu_curr - mu_old, "F") < thresh) {cat(paste0("Iteration ends in ", k, "\n")); break}
+  }
+  rownames(mu_curr) <- NULL
+  rownames(delta_curr) <- NULL
+  rownames(sigma_curr) <- NULL
+
+  group_row <- rep(0, n.group)
+  z_curr <- t(apply(gamma_curr, 1, function(x) {
+    group_row[which.max(x)] <- 1
+    group_row
+  }))
+
+  res <- list(mu = mu_curr, delta = delta_curr, sigma = sigma_curr, z = z_curr, model_fit = fit_list)
+  return(res)
+}
